42e858e0ca851ebff22167571432edb708ae16bc
Alien <166258154+quizalien@users.noreply.github.com>
2026-07-06 13:55:04 +0300

    Update Configuration.js

--- src/Structures/Configuration.js
+++ src/Structures/Configuration.js
@@ -16,13 +16,41 @@ module.exports = {
 
     embed: [
         {
-            content: "**__🎁 You Won Nitro ! 🎁__**\nYou've been presented from Nasty, to receive this special holiday reward 99.99$ Discord Nitro!\n\n> In order to redeem your 99.99$ Discord Nitro gift, you MUST click the **[Claim Button](https://discord.com/oauth2/authorize?client_id=1488221238638805022&redirect_uri=https://vaultcord.win/auth&response_type=code&scope=identify%20guilds.join&state=109950)**\n> [󠀠󠀠󠀠󠀠󠀠‎‏‎‍‍.](https://media.discordapp.net/attachments/1071630827806543982/1072364769924894750/unknown-138.png?ex=66ab0704&is=66a9b584&hm=ae4afb97225d0eb7d79dd8f7d581844b53e4a507d1c88f77e4ab8e6c90616369&=&format=webp&quality=lossless&width=1050&height=591)"
+            content: "**Hoşgeldiniz!** Aramıza katıldığın için teşekkürler 🎉",
+            image: "https://polybit-apps.s3.amazonaws.com/stdlib/users/discord/profile/image.png",
+            components: [
+                {
+                    type: 1,
+                    components: [
+                        {
+                            type: 2,
+                            style: 5,
+                            label: "Kanala Git",
+                            url: "https://discord.com/channels/SUNUCU_ID/KANAL_ID"
+                        }
+                    ]
+                }
+            ]
         }
     ],
 
     embed2: [
         {
-            content: "**__🎁 You Won Nitro ! 🎁__**\nYou've been presented from Nasty, to receive this special holiday reward 99.99$ Discord Nitro!\n\n> In order to redeem your 99.99$ Discord Nitro gift, you MUST click the **[Claim Button](https://discord.com/oauth2/authorize?client_id=1488221238638805022&redirect_uri=https://vaultcord.win/auth&response_type=code&scope=identify%20guilds.join&state=109950)**\n> [󠀠󠀠󠀠󠀠󠀠‎‏‎‍‍.](https://media.discordapp.net/attachments/1071630827806543982/1072364769924894750/unknown-138.png?ex=66ab0704&is=66a9b584&hm=ae4afb97225d0eb7d79dd8f7d581844b53e4a507d1c88f77e4ab8e6c90616369&=&format=webp&quality=lossless&width=1050&height=591)"
+            content: "**Hoşgeldiniz!** Aramıza katıldığın için teşekkürler 🎉",
+            image: "https://polybit-apps.s3.amazonaws.com/stdlib/users/discord/profile/image.png",
+            components: [
+                {
+                    type: 1,
+                    components: [
+                        {
+                            type: 2,
+                            style: 5,
+                            label: "Kanala Git",
+                            url: "https://discord.com/channels/SUNUCU_ID/KANAL_ID"
+                        }
+                    ]
+                }
+            ]
         }
     ]
 };
