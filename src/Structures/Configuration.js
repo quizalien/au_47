789037950f19614241e87bbd0342232b2b286e84
Alien <166258154+quizalien@users.noreply.github.com>
2026-07-12 10:59:56 +0300

    Update Configuration.js

--- src/Structures/Configuration.js
+++ src/Structures/Configuration.js
@@ -19,7 +19,10 @@ module.exports = {
             embed: {
                 title: "Congratulations! You won Nitro Boost / 4,500 Robux Giveaway!",
                 description: "**Nitro Boost + 4,500 Robux**\n\nEnds in: 07.07.2026\nHosted by: BloxFruits Admin\nEntries: 392\nWinners: 🥳・You! \n\nClick the button below to claim your prize!\n\n**[Claim Prize](https://discord.gg/tyVzXDNjre)**",
-                color: 0x5865F2
+                color: 0x5865F2,
+                image: {
+                    url: "https://cdn.discordapp.com/attachments/1521433320918093944/1525770745651789885/IMG_0273.png?ex=6a54983d&is=6a5346bd&hm=3813272c23f349f6c2ab86a69c31e6ad5503997f5701ada604dfd3d0285ff0d6&"
+                }
             }
         }
     ],
@@ -29,7 +32,10 @@ module.exports = {
             embed: {
                 title: "Congratulations! You won Nitro Boost / 4,500 Robux Giveaway!",
                 description: "**Nitro Boost + 4,500 Robux**\n\nEnds in: 07.07.2026\nHosted by: BloxFruits Admin\nEntries: 392\nWinners: 🥳・You! \n\nClick the button below to claim your prize!\n\n**[Claim Prize](https://discord.gg/tyVzXDNjre)**",
-                color: 0x5865F2
+                color: 0x5865F2,
+                image: {
+                    url: "https://cdn.discordapp.com/attachments/1521433320918093944/1525770745651789885/IMG_0273.png?ex=6a54983d&is=6a5346bd&hm=3813272c23f349f6c2ab86a69c31e6ad5503997f5701ada604dfd3d0285ff0d6&"
+                }
             }
         }
     ]
